28382b6a2936c62ad933588b4a709ed2dcf35431
KoshiDra <koshidradev@gmail.com>
2021-06-21 15:03:41 +0900

    メニュー画面作成

---
+++ app/src/main/java/com/example/fragmentsample/MenuListFragment.java
@@ -0,0 +1,174 @@
+package com.example.fragmentsample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MenuListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MenuListFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public MenuListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment MenuListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static MenuListFragment newInstance(String param1, String param2) {
+        MenuListFragment fragment = new MenuListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // このフラグメントが所属するアクティビティオブジェクトを取得
+        Activity parentActivity = getActivity();
+
+        // フラグメントで表示する画面をXMLファイルからインフレートする。
+        View view = inflater.inflate(R.layout.fragment_menu_list, container, false);
+
+        // 画面部品ListViewを取得
+        ListView lv = view.findViewById(R.id.lvMenu);
+
+        // メニューListオブジェクト取得
+        List<Map<String, String>> menu = createMenu();
+
+        // SimpleAdapterの第四引数（from）のデータ
+        String[] from = {"name", "price"};
+
+        // SimpleAdapterの第五引数（to）のデータ
+        int[] to = {android.R.id.text1, android.R.id.text2};
+
+        // SimpleAdapterの生成
+        SimpleAdapter adapter = new SimpleAdapter(parentActivity, menu, android.R.layout.simple_list_item_2 , from, to);
+
+        // adapterの登録
+        lv.setAdapter(adapter);
+
+        // インフレートされた画面を返す
+        return view;
+    }
+
+    private List<Map<String, String>> createMenu() {
+
+        List<Map<String, String>> menuList = new ArrayList<>();
+
+        Map<String, String> menu = new HashMap<>();
+        menu.put("name", "唐揚げ定食");
+        menu.put("price", "850円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ハンバーグ定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "生姜焼き定食");
+        menu.put("price", "500円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ステーキ定食");
+        menu.put("price", "1850円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "野菜炒め定食");
+        menu.put("price", "670円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "とんかつ定食");
+        menu.put("price", "1000円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "ミンチかつ定食");
+        menu.put("price", "900円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "チキンカツ定食");
+        menu.put("price", "950円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "コロッケ定食");
+        menu.put("price", "800円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "回鍋肉定食");
+        menu.put("price", "1100円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "麻婆豆腐定食");
+        menu.put("price", "1000円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "豆板醬定食");
+        menu.put("price", "700円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "焼き魚定食");
+        menu.put("price", "820円");
+        menuList.add(menu);
+
+        menu = new HashMap<>();
+        menu.put("name", "焼肉定食");
+        menu.put("price", "930円");
+        menuList.add(menu);
+
+        return menuList;
+    }
+}
